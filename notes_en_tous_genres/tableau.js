983cb31d723b7ee30d0d181f54cd7a008a282039
frederic-opdebeck <45725905+frederic-opdebeck@users.noreply.github.com>
2020-05-05 17:44:01 +0200

    Create tableau.js

---
+++ notes_en_tous_genres/tableau.js
@@ -0,0 +1,37 @@
+Enregistrement  -  1iere question :
+
+tab = 
+[
+    ['question' => 'tu vas bien ?'
+    ,
+    'reponse' =>  ['oui','non','je sais pas'],
+    'id' => '02082034'
+    ],
+
+    ['question' => 'quelle est ta couleur préférée ?', 
+    'reponse' =>  ['bleu','rouge','vert'],
+    'id' => 'uyft'
+    ],
+
+    ['question' => 'quelle est ta couleur préférée ?', 
+    'reponse' =>  ['bleu','rouge','vert'],
+    'id' => 'gflig'
+    ]
+]
+
+Lecture  -  1iere question :
+
+ tab [0] = ['question' => 'tu vas bien ?', 'reponse' =>  ['oui','non','je sais pas']]
+
+ tab[0]['question'] = 'tu vas bien ?'
+ tab[0]['reponse']  = ['oui','non','je sais pas']
+ tab[0]['reponse'] [0] = 'oui'
+ tab[0]['reponse'] [1] = 'non'
+ tab[0]['reponse'] [2] = 'je sais pas'
+
+
+ Q1>R1>Q2
+
+ Q1>R2>Q3
+
+ Q1>R3>Q4
