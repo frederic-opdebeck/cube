7e9b45c6a34cf771a3c584e1b49d1f424fd3d70e
frederic <fpubpoub@gmail.com>
2020-05-07 15:36:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/style.css">
-
+    <link rel="stylesheet" href="css/entete_question_reponses.css">    
     <title>CUBE</title>
 </head>
 <body>
