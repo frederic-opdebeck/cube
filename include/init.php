06dc0354504b0048d6f2f21691bee9c64833d0cb
frederic-opdebeck <45725905+frederic-opdebeck@users.noreply.github.com>
2020-05-07 14:22:39 +0200

    Update init.php

--- include/init.php
+++ include/init.php
@@ -4,5 +4,21 @@ require 'conf.php';
 
 require_once 'tools.php';
 
+//initialisation des class
+function classAutoload($className) {
+    $path = array('model/');
+    $extension = '.class.php';
+    // $className = strtolower($className);
+    $className = str_replace('\\', '/', $className);
+
+    foreach($path as $chemin) {
+        $fullPath = $chemin . $className . $extension;
+        if(file_exists($fullPath)) {
+            require_once($fullPath);
+        }
+    } 
+}
+spl_autoload_register('classAutoload');
+
 
 ?>
