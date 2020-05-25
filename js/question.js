9651ea797db06719bf3715fe71aa5f5d54e7685d
frederic <fpubpoub@gmail.com>
2020-05-25 14:22:42 +0200

    traitement de l'affichage Q/R

---
+++ js/question.js
@@ -0,0 +1,28 @@
+const modele_questions = 
+// deux premieres questions path = qn>ri>qn+1->ri+1
+    [{
+            'question' : 'tu vas bien ?',
+            'reponse' :  {'R1' :'oui','R2' : 'non', etc, 'R-n' : 'nnn'},
+            'id' : '02082034',
+            'path' : 'Q0'
+        },
+        {
+            'question' : 'Vas-tu aller faire ton jogging ?', 
+            'reponse' :  {'R1' :'oui','R2' : 'non', etc, 'R3' : 'Je ne sais pas'},
+            'id' : 'uyft',
+            'path' : 'Q0>R1>Q1'
+        },
+        {
+            'question' : 'Vas-tu aller voir le docteur ?', 
+            'reponse' :  {'R1' :'oui','R2' : 'non'},
+            'id' : 'gflig',
+            'path' : 'Q0>R2>Q2'
+        },
+        {etc},
+        {
+            'question' : 'Vas-tu ale ton jogging ?', 
+            'reponse' :  {'R1' :'oui','R2' : 'non'},
+            'id' : 'uyft',
+            'path' : 'Q0>R-n>Q-n'
+        }
+    ]
--- questionnaire.php
+++ questionnaire.php
@@ -1,6 +1,10 @@
-<?php 
-require 'include/header.php';
-?>
+<?php require 'include/header.php';?>
+
 <h1>Page Questionnaire</h1>
 <hr>
 <hr>
+
+<p id="question">question</p>
+
+<section id="reponse">
+</section>
