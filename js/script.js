7fd009b7aec6290e49f7cdc545fd4edc7ba7e3cb
frederic <fpubpoub@gmail.com>
2020-05-28 14:40:14 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+function printresults(){
+    console.log('coucou')
+}
 let recap = [];
 const questions = {
     'q1':{
             'question': 'Habitez-vous vous en France ?', // Do you leave
             'reponses': { 'R1': 'Oui', 'R2': 'Non'},
-            'img' : 'img_1.jgp',
-            'type': 'button',
+            'img' : ['img_1.jgp'],
+            'type': 'radio',
             'suite':function (rep){
                 if (rep.includes('R1')){
                     return writequestion('q2');
@@ -16,8 +19,8 @@ const questions = {
     'q2':{
             'question': 'A quoi servira votre CUBE ?', // What will you be using your Cube for ?
             'reponses': { 'R1': '1','R2': '2','R3': '3','R4': '4','R5': '5','R6': '6' },
-            'img' : 'img_4.jgp',
-            'type': 'button',
+            'img' : ['img_4.jgp'],
+            'type': 'radio',
             'suite': function (rep){
                     return writequestion('q4');
             }
@@ -25,22 +28,22 @@ const questions = {
     'q3':{
             'question': 'Fin du questionnaire', // Is this the property you're looking for ?
             'reponses': null,
-            'img' : 'img_2.jgp',
+            'img' : ['img_2.jgp'],
             'type': null,
             'suite': null
         },
     'q4':{
             'question': 'Voulez-vous : une baignoire ou une cuisine ?', // 'Do you want a Kitchen or Bathroom?',
             'reponses': { 'R1': 'Une baignoire', 'R2': 'Une cuisine'},
-            'img' : 'img_5.jgp',
-            'type': 'button',
+            'img' : ['img_5.jgp'],
+            'type': 'radio',
             'suite':function (rep){
                     return writequestion('q5');
             }
         },
     'q5':{
             'question': 'Quelle est votre surface préférée ?', 
-            'reponses': { 'R1': 'numbre_m2'},
+            'reponses': { 'R1' : 'm2'},
             'img' : null,
             'type': 'number',
             'suite':function (rep){
@@ -50,8 +53,8 @@ const questions = {
     'q6':{
             'question': 'Avez-vous au moins une construction sur votre terrain ?', // Do you have any Construction in your Backyard? (Image 6)
             'reponses': { 'R1': 'Oui', 'R2': 'Non'},
-            'img' : 'img_6',
-            'type': 'button',
+            'img' : ['img_6'],
+            'type': 'radio',
             'suite':function (rep){
                 if (rep.includes('R1')){
                     return writequestion('q7');
@@ -63,8 +66,8 @@ const questions = {
     'q7':{
         'question': 'Voulez-vous détruire cette construction ?', // Would you like to remove this structure? (Image 7)
         'reponses': { 'R1': 'Oui', 'R2': 'Non'},
-        'img' : 'img_7',
-        'type': 'button',
+        'img' : ['img_7'],
+        'type': 'radio',
         'suite':function (rep){
                 return writequestion('q8');   
         }
@@ -72,27 +75,39 @@ const questions = {
     'q8':{
         'question': 'Quelle type de consctruction aimez-vous le plus ?', // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'img_11 en bg', 'R2': 'img_12 en bg', 'R3' :'img_13 en bg'},
-        'img' : 'img_11.jpg-img_12.jpg-img_13.jpg',
+        'img' : ['img_11.jpg','img_12.jpg','img_13.jpg'],
         'type': 'button',
         'suite':function (rep){
+            printresults(recap);
             return writequestion('q9');   
-    }
+        }
         // fonction pour Récapituler de toutes les q et r associées + envoie d"un mail et proposer envoie de mail telechargement de la config'
     },
     'q9':{
         'question': 'Voulez-vous sauvegarder votre rapport ?', // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'oui', 'R2': 'non'},
-        'img' : 'img_11.jpg-img_12.jpg-img_13.jpg',
-        'type': 'button',
+        'img' : ['img_9.jpg'],
+        'select': { 'S1': ''},
         'suite':function (rep){
-            return writequestion('q10');   
-    }
+
+            return writequestion('q9bis');   
+        }
         // fonction qui enoive un mail de récap au client potentiel
+    },
+    'q9bis':{
+        'question': "Voulez-vous nous l'envoyer par email ?", // Which look do you like most? (Image 11-12-13)
+        'reponses': { 'R1': 'oui', 'R2': 'non'},
+        'img' : ['img_9.jpg'],
+        'type': 'radio',
+        'suite':function (rep){
+            printResults()
+            return writequestion('q10');   
+        }
     },
     'q10':{
         'question': 'Achetez-vous votre CUBE ?', // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'oui', 'R2': 'non'},
-        'img' : 'img_11.jpg-img_12.jpg-img_13.jpg',
+        'img' : ['img_17.jpg','img_18.jpg'],
         'type': 'button',
         'suite':function (rep){
             if (rep.includes('R1')){
