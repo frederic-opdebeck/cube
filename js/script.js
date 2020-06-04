85d7778c8d7595ce5e33432ca89a7b289e35846b
dutreix <morgandutreix@hotmail.fr>
2020-06-04 16:15:14 +0200

    a
    a

--- js/script.js
+++ js/script.js
@@ -28,10 +28,27 @@ function printResults(r){
             
     }
 }
+function ajaxQuestion(id){
+    let idQ = id.split('q');
+    let dataQ;
+    $.ajax({
+        url: "ajaxQuestionReponse.php",
+        type: "POST",
+        async: false, // Mode synchrone
+        data: ({
+            id_question: idQ[1]
+        }),
+        complete: function(data){
+            dataQ = data;
+            return dataQ;
+        }
+    });
+    return dataQ.responseText;
+}
 let recap = [];
 const questions = {
     'q0':{
-        'question': 'Saisissez une adresse', // Do you leave
+        'question': ajaxQuestion('q1'), // Do you leave
         'reponses': { 'R1': 'Valider mon adresse'},
 
         'img' : ['q0_0.jgp'],
@@ -50,7 +67,7 @@ const questions = {
         }
     },
     'q1':{
-            'question': 'Est-ce le terrain que vous recherchez?', // Do you leave
+            'question': ajaxQuestion('q2'), // Do you leave
             'reponses': { 'R1': 'Oui', 'R2': 'Non'},
             'img' : ['q1_0.jgp'],
             'type': 'radio',
@@ -63,7 +80,7 @@ const questions = {
             }
         },
     'q2':{
-            'question': 'A quoi servira votre CUBE ?', // What will you be using your Cube for ?
+            'question': ajaxQuestion('q3'), // What will you be using your Cube for ?
             'reponses': { 'R1': "Chambre d'hôte",'R2': 'Commerce','R3': 'Demenagement','R4': 'Bureau','R5': 'Piscine','R6': 'Autre' },
             'img': ['q2_0.jpg'],
             'type': 'radio',
@@ -72,14 +89,14 @@ const questions = {
             }
         },
     'q3':{
-            'question': 'Fin du questionnaire', // Is this the property you're looking for ?
+            'question': ajaxQuestion('q4'), // Is this the property you're looking for ?
             'reponses': null,
             'img' : ['q3_0.jgp'],
             'type': null,
             'suite': null
         },
     'q4':{
-            'question': 'Voulez-vous une salle de bain ou une cuisine ?', // 'Do you want a Kitchen or Bathroom?',
+            'question': ajaxQuestion('q5'), // 'Do you want a Kitchen or Bathroom?',
             'reponses': { 'R1': 'Un cuisine et une salle de bain', 'R2': 'Seulement une salle de bain', 'R3': 'Aucune'},
             'img' : ['q4_0.jgp'],
             'type': 'radio',
@@ -88,7 +105,7 @@ const questions = {
             }
         },
     'q5':{
-            'question': 'Avez vous une autre structure sur votre terrain?', 
+            'question': ajaxQuestion('q6'), 
             'reponses': { 'R1' : 'Oui', 'R2' : 'Non'},
             'img' : null,
             'type': 'number',
@@ -104,7 +121,7 @@ const questions = {
     // PENSER A DEMANDER SI IL Y A PLUSIEURS STRUCTURE SUR LE TERRAIN 
 
     'q6':{
-            'question': 'Quel type de structure est-ce?', // Do you have any Construction in your Backyard? (Image 6)
+            'question': ajaxQuestion('q7'), // Do you have any Construction in your Backyard? (Image 6)
             'reponses': { 'R1': 'Garage', 'R2': 'Espace de vie avec plomberie', 'R3' : 'Espace de vie sans plomberie', 'R4' : 'Autre'},
             'img' : ['q6_0'],
             'type': 'radio',
@@ -113,7 +130,7 @@ const questions = {
                 }
     },
     'q7':{
-        'question': 'Voulez-vous détruire cette structure ?', // Would you like to remove this structure? (Image 7)
+        'question': ajaxQuestion('q8'), // Would you like to remove this structure? (Image 7)
         'reponses': { 'R1': 'Oui', 'R2': 'Non'},
         'img' : ['q7_0'],
         'type': 'radio',
@@ -122,7 +139,7 @@ const questions = {
         }
     },
     'q8':{
-        'question': 'Quelle type de consctruction aimez-vous le plus ?', // Which look do you like most? (Image 11-12-13)
+        'question': ajaxQuestion('q9'), // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'img_11 en bg', 'R2': 'img_12 en bg', 'R3' :'img_13 en bg'},
         'img' : ['q8_0.jpg','q8_1.jpg','q8_2.jpg'],
         'type': 'radio',
@@ -133,7 +150,7 @@ const questions = {
         // fonction pour Récapituler de toutes les q et r associées + envoie d"un mail et proposer envoie de mail telechargement de la config'
     },
     'q9':{
-        'question': 'Voulez-vous sauvegarder votre rapport ?', // Which look do you like most? (Image 11-12-13)
+        'question': ajaxQuestion('q10'), // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'oui', 'R2': 'non'},
         'img' : ['q9_0.jpg'],
         'type': 'radio',
@@ -147,7 +164,7 @@ const questions = {
         // fonction qui enoive un mail de récap au client potentiel
     },
     'q9bis':{
-        'question': "Voulez-vous nous l'envoyer par email ?", // 
+        'question': ajaxQuestion('q11'), // 
         'reponses': { 'R1': 'oui', 'R2': 'non'},
         'img' : ['q9bis_0.jpg'],
         'type': 'radio',
@@ -156,7 +173,7 @@ const questions = {
         }
     },
     'q10':{
-        'question': 'Achetez-vous votre CUBE ?', // Which look do you like most? (Image 11-12-13)
+        'question': ajaxQuestion('q12'), // Which look do you like most? (Image 11-12-13)
         'reponses': { 'R1': 'oui', 'R2': 'non'},
         'img' : ['q10_0.jpg','q10_1.jpg'],
         'type': 'button',
@@ -169,7 +186,7 @@ const questions = {
         }
     },
     'q11':{
-        'question': 'envoie vers une page de paiement', // Which look do you like most? (Image 11-12-13)
+        'question': ajaxQuestion('q13'), // Which look do you like most? (Image 11-12-13)
         'reponses': null,
         'img' : null,
         'type': 'button',
@@ -184,7 +201,7 @@ function writequestion(q){
         quest.textContent = questions[q].question;
         if(document.querySelectorAll('.buttonR')) {
             document.querySelectorAll(".buttonR").forEach(e => e.parentNode.removeChild(e));
-            }  
+            }
     }else{
         if(q==='q0') {
             questions[q].carte();
@@ -198,17 +215,34 @@ function writequestion(q){
     let nbr = 1
     if(document.querySelectorAll('.buttonR')) {
         document.querySelectorAll(".buttonR").forEach(e => e.parentNode.removeChild(e));
-        }  
+        } 
     for (const key in questions[q].reponses) {
         if (questions[q].reponses.hasOwnProperty(key)) {
                 let r = document.createElement("button");
                 let myClass = document.getElementById('questionReponses').classList;
                 r.setAttribute('id','R' + nbr)
-                r.setAttribute('class', 'buttonR')
+                r.setAttribute('class', 'buttonR');
+                if(q==='q0') {
+                    window.onload = function(){
+                        let rechercheInput = document.querySelector("input[placeholder='Rechercher un lieu, une adresse']");
+                        console.log(rechercheInput)
+                        if(rechercheInput.value === '') {
+                            r.style.pointerEvents = 'none';
+                        }
+                        rechercheInput.addEventListener('change', () => {
+                            if(rechercheInput.value!=='') {
+                                r.style.pointerEvents = 'auto';
+                                r.style.cursor = 'pointer';
+                            }
+                            else {
+                                r.style.pointerEvents = 'none';
+                            }
+                        });
+                    };
+                }
                 r.textContent = questions[q].reponses[key];
                 document.getElementById('questionReponses').classList.replace(myClass, q);
                 document.getElementById('questionReponses').appendChild(r);
-                console.log(questions[q].class);
                 r.addEventListener("click", function(){
                     if(q!=='q0') {   
                     recap.push({ 'laQuestion' : questions[q].question , 'laReponse' : questions[q].reponses[r.id]});
@@ -226,8 +260,16 @@ function writequestion(q){
 };
 
 writequestion('q0');
-
-
+if (typeof(Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function() {
+      return this * Math.PI / 180;
+    }
+  }
+function getTileURL(lat, lon, zoom) {
+    var xtile = parseInt(Math.floor( (lon + 180) / 360 * (1<<zoom) ));
+    var ytile = parseInt(Math.floor( (1 - Math.log(Math.tan(lat.toRad()) + 1 / Math.cos(lat.toRad())) / Math.PI) / 2 * (1<<zoom) ));
+    return "" + zoom + "/" + xtile + "/" + ytile;
+}
 function go() {
     map = L.map("mapid",{crs : L.CRS.EPSG4326,minZoom: 10, maxZoom:18}).setView([48.845, 2.424], 10);
     L.geoportalLayer.WMS({
@@ -235,7 +277,7 @@ function go() {
     }, { // leafletParams
       opacity: 0.7,
     }).addTo(map);
-    var searchCtrl = L.geoportalControl.SearchEngine({'zoomTo':18});
+    var searchCtrl = L.geoportalControl.SearchEngine({'zoomTo':18, 'displayAdvancedSearch':false, 'collapsed':false});
     map.addControl(searchCtrl);
     
     L.geoportalLayer.WMS({
@@ -245,23 +287,17 @@ function go() {
         // ne sont pas bien renseignes dans l'autoconf (en cours de correction)
         styles : "inspire_common:DEFAULT",
         transparent : true,
-    }).addTo(map);
-
-    L.geoportalLayer.WMS({
-      layer: "TN.RoadTransportNetwork",
-    },{
-        styles : "inspire_common:DEFAULT",
-        transparent : true,
-    }).addTo(map);
-
-    L.geoportalLayer.WMS({
-      layer: "HY.PhysicalWaters",
-    },{
-        styles : "inspire_common:DEFAULT",
-        transparent : true,
     }).on('load', () => {
-        let img = document.getElementById('imgChargement');
-        img.remove();
+        if(document.getElementById('imgChargement')){
+            let img = document.getElementById('imgChargement');
+            img.remove();
+
+        }
        }).addTo(map);
+    map.on('click', (e) => {
+        
+        console.log(getTileURL(e.latlng.lat, e.latlng.lng, map.getZoom()));
+    })
     
   }
+  
